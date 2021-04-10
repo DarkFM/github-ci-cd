dad7522fcc4ac8a63cb1ca6dbeeedb56750fc400
Clinton <c.bestmann@hotmail.com>
2021-04-10 12:35:30 -0700

    remove https redirect

--- AnimalCountingDatabase/Startup.cs
+++ AnimalCountingDatabase/Startup.cs
@@ -44,7 +44,7 @@ namespace AnimalCountingDatabase
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalCountingDatabase v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
